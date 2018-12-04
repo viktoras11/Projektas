8f265d841f277ef4fe3a8f0ef3b155c95c219faf
viktoras <you@example.com>
2018-12-04 14:18:13 +0200

    init commit

---
+++ src/juruMusis/juruMusis1.java
@@ -0,0 +1,13 @@
+package juruMusis;
+
+public class juruMusis1 {
+
+	public static void main(String[] args) {
+
+		String[][] board = new String[10][10];
+		juruMusis.createBoard(board);
+		juruMusis.showBoard(board);
+		juruMusis.showBoard(board);
+	}
+
+}
